de7196e5f5be01bca6839c5ef3706ca33052ece0
TheBrotherFromASouthernMother <christglowe@gmail.com>
2018-01-10 01:49:17 -0600

    Testing for each hour

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var countingVariable = 0;
 console.log(jokes.length);
 
 
-let textJob = new cronJob('1-59 0-23 * * 1-5', function () {
+let textJob = new cronJob('0 0-23 * * 1-5', function () {
     msg = jokes[countingVariable].joke;
     if (countingVariable < jokes.length) {
         countingVariable ++;
